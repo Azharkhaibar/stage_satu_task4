e5819487dbe2ca0f58917a42999d2e0f9a71a99f
ASUS <azharasykari@gmail.com>
2024-07-21 11:45:08 +0700

    ini baru

---
+++ jsday1/day1.js
@@ -0,0 +1,50 @@
+let nSaya = "Azhar aja";
+console.log(`nama syaa : ${nSaya}, ${typeof nSaya}`);
+
+let angka = 0;
+let a = (500 + 2) * angka + (2 + 1);
+console.log(a);
+
+// perbaikan
+
+function on_alert(...y) {
+  let x = [];
+  if (x !== y) {
+    x = y;
+    for (let i = 0; i < x.length; i++) {
+      console.log(`data : ${x[i]} , tes dulu`);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+ 
+  const contactForm = document.getElementById("contactForm");
+
+
+  if (contactForm) {
+    
+    contactForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+     
+      let nama = document.getElementById("name").value;
+      let email = document.getElementById("email").value;
+      let phoneNumber = document.getElementById("phone").value;
+      let subject = document.getElementById("subject").value;
+      let message = document.getElementById("message").value;
+
+
+      console.log(`Nama: ${nama}`);
+      console.log(`Email: ${email}`);
+      console.log(`Phone: ${phoneNumber}`);
+      console.log(`Subject: ${subject}`);
+      console.log(`Message: ${message}`);
+
+     
+      contactForm.reset();
+    });
+  } else {
+    console.error('Form with ID "contactForm" not found.');
+  }
+});
